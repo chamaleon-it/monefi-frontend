05ccb3aee94c720b104386fe6c051fec8050d5c8
rashidpkn <muhammed916rashid@gmail.com>
2025-07-05 16:44:49 +0530

    Hello

--- src/app/application-form-2/components/stage/ApplicationFormCompleted.tsx
+++ src/app/application-form-2/components/stage/ApplicationFormCompleted.tsx
@@ -32,7 +32,7 @@ export default function ApplicationFormCompleted({ formData }: StartProps) {
   return (
     <div className="h-full w-full lg:w-1/2 bg-monefi-pink py-10 px-5 flex flex-col items-center justify-center gap-5 text-monefi-black">
       <h2 className="text-2xl font-semibold text-center">Application form completed</h2>
-      <p className="text-center text-center">Review and submit your application.</p>
+      <p className="text-center">Review and submit your application.</p>
 
       <button className="text-monefi-pink px-4 py-2 rounded-lg bg-monefi-black hover:px-5 duration-200 cursor-pointer hover:opacity-90" onClick={()=>completeApplicationForm(formData)}>
         Submit
