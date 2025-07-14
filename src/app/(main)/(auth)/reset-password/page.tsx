7aec4ade9fd09e23e35703ed98c7961d8012b643
rashidpkn <muhammed916rashid@gmail.com>
2025-07-14 16:13:41 +0530

    Reset password page implemented

---
+++ src/app/(main)/(auth)/reset-password/page.tsx
@@ -0,0 +1,8 @@
+import ResetPasswordForm from '@/sections/reset-password/ResetPasswordForm'
+import React from 'react'
+
+export default function page() {
+  return (
+    <ResetPasswordForm/>
+  )
+}
