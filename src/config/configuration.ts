a3cc84a6b62a85add50c77d9562c3fe336fcf5a7
rashidpkn <muhammedrashid@gmail.com>
2026-06-12 09:36:53 +0530

    feat: add configuration utility to manage environment variables and constants

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 const getConfig = () => ({
   backendURL: process.env.NEXT_PUBLIC_BASE_URL,
-  twelveDataApiKey :process.env.NEXT_PUBLIC_TWELVE_DATA_API_KEY,
-  exchangeRateUSDtoGBP :0.73
+  twelveDataApiKey: process.env.NEXT_PUBLIC_TWELVE_DATA_API_KEY,
+  exchangeRateUSDtoGBP: 0.75
 });
 
 export default getConfig;
