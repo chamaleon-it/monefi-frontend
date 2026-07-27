e6c3a605d5e42d177f8d40fa3ffb0b1cee7d02f1
rashidpkn <muhammedrashid@gmail.com>
2026-07-27 11:56:33 +0100

    feat: add useDashboardLinks hook to manage role-based sidebar navigation items

--- src/layout/dashboard/useDashboardLinks.ts
+++ src/layout/dashboard/useDashboardLinks.ts
@@ -97,11 +97,11 @@ export default function useDashboardLinks(): Links[] {
       path: paths.dashboard.user.ipos,
       icon: "mdi:bank-outline",
     },
-    {
-      title: "July Recommendation",
-      path: "/july-bond-recommendation-summary",
-      icon: "mingcute:file-certificate-line",
-    },
+    // {
+    //   title: "July Recommendation",
+    //   path: "/july-bond-recommendation-summary",
+    //   icon: "mingcute:file-certificate-line",
+    // },
     {
       title: "My Portfolio",
       path: paths.dashboard.user.myPortfolio,
